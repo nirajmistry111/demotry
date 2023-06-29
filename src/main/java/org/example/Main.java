ea4eff56aeb9424ce85cd7844e25a96268a95bdf
Niraj Mistry <niraj.mistry@dcmail.ca>
2023-06-29 11:38:01 -0500

    version 1.0

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        Date d = new Date();
+        ZoneId z = ZoneId.systemDefault();
+        System.out.println(c.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+        System.out.println(d.getDate());
+
+
+    }
+    }
